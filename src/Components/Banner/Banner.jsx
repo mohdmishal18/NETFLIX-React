2341c7967c00a57c2552393e1c44e82732ff409e
Mohammed Mishal <mohdmishal18@gmail.com>
2024-05-12 10:48:48 +0530

    something

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Banner.css'
+import axios from '../../axios'
+import {API_KEY} from '../../constants/constants'
 function Banner()
 {
+  const [state, setstate] = useState()
+  useEffect(() =>
+  {
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`)
+    .then((response) =>
+    {
+      console.log(response.data.results[0])
+    })
+  },[])
   return (
     <div className='banner'>
         <div className='content'>
---
+++ src/constants/constants.js
@@ -0,0 +1,2 @@
+export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY = '75094105cfde88f311873ed2748fb7ba'
